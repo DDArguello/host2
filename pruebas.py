b5e5d3ad13777ce80a5e0f3de7a86bb2bc7abbd4
DDArguello <portagame1234@gmail.com>
2022-01-01 17:25:39 -0500

    Add files via upload

---
+++ pruebas.py
@@ -0,0 +1,15 @@
+
+num = 1320
+x = [i for i in str(num)]
+print(x)
+lista=['']*2
+lista [0]= x[0]+x[1]
+lista [1]= x[2]+x[3]
+print(lista)
+Hora = int(lista[0])*4
+minutos=int(lista[1])
+if minutos>30:
+    Hora+=2.50
+    print(f"Su valor a pagar es de: {Hora}" )
+else:
+    print(f"Su valor a pagar es de: {Hora}" )
